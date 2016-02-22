39a002ae10abf271338fc8789ce7171dbc83c683
Jacky Zhang <jacky_02468@hotmail.com>
2016-02-22 22:34:04 +0000

    added textures to objects

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -113,7 +113,7 @@ function init() {
         scene.add(sphere);*/
     //Add a Sphere (planet vegeta)
     vegeta = new SphereGeometry(3, 10, 10);
-    vegetaMaterial = new LambertMaterial({ color: 0x0B2161 });
+    vegetaMaterial = new LambertMaterial({ color: 0x0B2161, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/metal.jpg") });
     vegeta = new Mesh(vegeta, vegetaMaterial);
     vegeta.castShadow = true;
     vegeta.position.x = 0;
@@ -123,7 +123,7 @@ function init() {
     scene.add(planetVegeta);
     //Add a Sphere (planet namek)
     namek = new SphereGeometry(4, 16, 16);
-    namekMaterial = new LambertMaterial({ color: 0x3ADF00 });
+    namekMaterial = new LambertMaterial({ color: 0x3ADF00, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/forest.jpg") });
     namek = new Mesh(namek, namekMaterial);
     namek.castShadow = true;
     namek.position.x = 0;
@@ -133,7 +133,7 @@ function init() {
     scene.add(planetNamek);
     //Add a Sphere (kai's planet)
     kai = new SphereGeometry(2, 5, 5);
-    kaiMaterial = new LambertMaterial({ color: 0xF7FE2E });
+    kaiMaterial = new LambertMaterial({ color: 0xF7FE2E, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/fluffy.jpg") });
     kai = new Mesh(kai, kaiMaterial);
     kai.castShadow = true;
     kai.position.x = 0;
@@ -143,7 +143,7 @@ function init() {
     scene.add(planetKai);
     //Add a Sphere (planet hera)
     hera = new SphereGeometry(2, 5, 5);
-    heraMaterial = new LambertMaterial({ color: 0xFE2E64 });
+    heraMaterial = new LambertMaterial({ color: 0xFE2E64, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/bones.jpg") });
     hera = new Mesh(hera, heraMaterial);
     hera.castShadow = true;
     hera.position.x = 0;
@@ -173,7 +173,7 @@ function init() {
     scene.add(planetFriezaPivot);
     //Add a Sphere (frieza's moon)
     fmoon79 = new SphereGeometry(1, 10, 10);
-    moon79Material = new LambertMaterial({ color: 0xF8E0E0 });
+    moon79Material = new LambertMaterial({ color: 0xF8E0E0, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/foam.jpg") });
     fmoon79 = new Mesh(fmoon79, moon79Material);
     fmoon79.castShadow = true;
     fmoon79.position.x = 0;
