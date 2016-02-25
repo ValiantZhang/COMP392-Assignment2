d5c9db9f975bf3c26c60af9616c4959e01358039
Jacky Zhang <jacky_02468@hotmail.com>
2016-02-25 18:09:59 +0000

    revered back to original light settings

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -67,7 +67,6 @@ function init() {
     // add an axis helper to the scene
     axes = new AxisHelper(20);
     scene.add(axes);
-    console.log("Added Axis Helper to scene...");
     //Add a Plane to the Scene
     /*    plane = new gameObject(
             new PlaneGeometry(60, 40, 1, 1),
@@ -90,139 +89,110 @@ function init() {
     moon79 = new THREE.Object3D();
     //Add a Sphere (sun)
     sun = new SphereGeometry(5, 50, 50);
-    sunMaterial = new LambertMaterial({ color: 0xffff66, emissive: 0x808000, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/gas.png") });
-    sun = new Mesh(sun, sunMaterial);
-    sun.castShadow = true;
-    sun.position.x = 0;
-    sun.position.y = 0;
-    sun.position.z = 0;
-    //Add Light to the Sun
-    pointLight = new PointLight(0xffffAA, 10.0, 100);
-    pointLight.position.set(0, 0, 0);
-    pointLight.castShadow = true;
-    sun.add(pointLight);
-    scene.add(sun);
-    /*    //Add a Sphere ()
-        sphere = new SphereGeometry(5, 10, 10);
-        sphereMaterial = new LambertMaterial({ color: 0x000000 });
-        sphere = new Mesh(sphere, sphereMaterial);
-        sphere.castShadow = true;
-        sphere.position.x = 0;
-        sphere.position.y = 30;
-        sphere.position.z = 0;
-        scene.add(sphere);*/
-    //Add a Sphere (planet vegeta)
-    vegeta = new SphereGeometry(3, 10, 10);
-    vegetaMaterial = new LambertMaterial({ color: 0xBADAEB, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/metal.jpg") });
-    vegeta = new Mesh(vegeta, vegetaMaterial);
-    vegeta.castShadow = true;
-    vegeta.position.x = 0;
-    vegeta.position.y = 10;
-    vegeta.position.z = 50;
-    planetVegeta.add(vegeta);
-    scene.add(planetVegeta);
-    //Add a Sphere (planet namek)
-    namek = new SphereGeometry(4, 16, 16);
-    namekMaterial = new LambertMaterial({ color: 0x3ADF00, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/forest.jpg") });
-    namek = new Mesh(namek, namekMaterial);
-    namek.castShadow = true;
-    namek.position.x = 0;
-    namek.position.y = 5;
-    namek.position.z = 15;
-    planetNamek.add(namek);
-    scene.add(planetNamek);
-    //Add a Sphere (kai's planet)
-    kai = new SphereGeometry(2, 5, 5);
-    kaiMaterial = new LambertMaterial({ color: 0xF7FE2E, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/fluffy.jpg") });
-    kai = new Mesh(kai, kaiMaterial);
-    kai.castShadow = true;
-    kai.position.x = 0;
-    kai.position.y = -25;
-    kai.position.z = 25;
-    planetKai.add(kai);
-    scene.add(planetKai);
-    //Add a Sphere (planet hera)
-    hera = new SphereGeometry(2, 5, 5);
-    heraMaterial = new LambertMaterial({ color: 0xFE2E64, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/bones.jpg") });
-    hera = new Mesh(hera, heraMaterial);
-    hera.castShadow = true;
-    hera.position.x = 0;
-    hera.position.y = 0;
-    hera.position.z = 40;
-    planetHera.add(hera);
-    //Add Hera's Ring
-    heraRing = new RingGeometry(4, 0.7, 2, 30);
-    heraRingMaterial = new LambertMaterial({ color: 0xFE2E64, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/asteroidBelt.jpg") });
-    heraRing = new Mesh(heraRing, heraRingMaterial);
-    heraRing.castShadow = true;
-    heraRing.rotation.x = 90;
-    heraRing.position.z = 40;
-    planetHeraRing.add(heraRing);
-    planetHera.add(planetHeraRing);
-    scene.add(planetHera);
-    //Add a Sphere (planet frieza)
-    frieza = new SphereGeometry(4, 8, 8);
-    friezaMaterial = new LambertMaterial({ color: 0x5882FA, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/ice.jpg") });
-    frieza = new Mesh(frieza, friezaMaterial);
-    frieza.castShadow = true;
-    planetFrieza.position.x = 0;
-    planetFrieza.position.y = 0;
-    planetFrieza.position.z = 30;
-    planetFrieza.add(frieza);
-    planetFriezaPivot.add(planetFrieza);
-    scene.add(planetFriezaPivot);
-    //Add a Sphere (frieza's moon)
-    fmoon79 = new SphereGeometry(1, 10, 10);
-    moon79Material = new LambertMaterial({ color: 0xF8E0E0, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/foam.jpg") });
-    fmoon79 = new Mesh(fmoon79, moon79Material);
-    fmoon79.castShadow = true;
-    fmoon79.position.x = 0;
-    fmoon79.position.y = 0;
-    fmoon79.position.z = 7;
-    moon79.add(fmoon79);
-    planetFrieza.add(moon79);
-    // Add an AmbientLight to the scene
-    ambientLight = new AmbientLight(0x0c0c0c);
-    scene.add(ambientLight);
-    console.log("Added an Ambient Light to Scene");
-    /*    // Add a SpotLight to the scene
-        spotLight = new SpotLight(0xffffff, 1.0, Math.PI/3);
-        spotLight.position.set(-500, 0, 0);
-        spotLight.castShadow = true;
-        scene.add(spotLight);
-        console.log("Added a SpotLight Light to Scene");*/
-    // add controls
-    gui = new GUI();
-    control = new Control(0.0, 0.0, 0.0);
-    addControl(control);
-    console.log("Added Control to scene...");
-    // Add framerate stats
-    addStatsObject();
-    console.log("Added Stats to scene...");
-    document.body.appendChild(renderer.domElement);
-    gameLoop(); // render the scene	
-    window.addEventListener('resize', onResize, false);
+    sunMaterial = new LambertMaterial({ color: 0x808000, emissive: 0x808000, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/gas.png") });
 }
+;
+sun = new Mesh(sun, sunMaterial);
+sun.castShadow = true;
+sun.position.x = 0;
+sun.position.y = 0;
+sun.position.z = 0;
+//Add Light to the Sun
+pointLight = new PointLight(0xffff00, 1.0, 100);
+pointLight.position.set(0, 0, 0);
+pointLight.castShadow = true;
+sun.add(pointLight);
+scene.add(sun);
+//Add a Sphere (planet vegeta)
+vegeta = new SphereGeometry(3, 10, 10);
+vegetaMaterial = new LambertMaterial({ color: 0xBADAEB, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/metal.jpg") });
+vegeta = new Mesh(vegeta, vegetaMaterial);
+vegeta.castShadow = true;
+vegeta.position.x = 0;
+vegeta.position.y = 10;
+vegeta.position.z = 50;
+planetVegeta.add(vegeta);
+scene.add(planetVegeta);
+//Add a Sphere (planet namek)
+namek = new SphereGeometry(4, 16, 16);
+namekMaterial = new LambertMaterial({ color: 0x3ADF00, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/forest.jpg") });
+namek = new Mesh(namek, namekMaterial);
+namek.castShadow = true;
+namek.position.x = 0;
+namek.position.y = 5;
+namek.position.z = 15;
+planetNamek.add(namek);
+scene.add(planetNamek);
+//Add a Sphere (kai's planet)
+kai = new SphereGeometry(2, 5, 5);
+kaiMaterial = new LambertMaterial({ color: 0xF7FE2E, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/fluffy.jpg") });
+kai = new Mesh(kai, kaiMaterial);
+kai.castShadow = true;
+kai.position.x = 0;
+kai.position.y = -25;
+kai.position.z = 25;
+planetKai.add(kai);
+scene.add(planetKai);
+//Add a Sphere (planet hera)
+hera = new SphereGeometry(2, 5, 5);
+heraMaterial = new LambertMaterial({ color: 0xFE2E64, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/bones.jpg") });
+hera = new Mesh(hera, heraMaterial);
+hera.castShadow = true;
+hera.position.x = 0;
+hera.position.y = 0;
+hera.position.z = 40;
+planetHera.add(hera);
+//Add Hera's Ring
+heraRing = new RingGeometry(4, 0.7, 2, 30);
+heraRingMaterial = new LambertMaterial({ color: 0xFE2E64, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/asteroidBelt.jpg") });
+heraRing = new Mesh(heraRing, heraRingMaterial);
+heraRing.castShadow = true;
+heraRing.rotation.x = 90;
+heraRing.position.z = 40;
+planetHeraRing.add(heraRing);
+planetHera.add(planetHeraRing);
+scene.add(planetHera);
+//Add a Sphere (planet frieza)
+frieza = new SphereGeometry(4, 8, 8);
+friezaMaterial = new LambertMaterial({ color: 0x5882FA, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/ice.jpg") });
+frieza = new Mesh(frieza, friezaMaterial);
+frieza.castShadow = true;
+planetFrieza.position.x = 0;
+planetFrieza.position.y = 0;
+planetFrieza.position.z = 30;
+planetFrieza.add(frieza);
+planetFriezaPivot.add(planetFrieza);
+scene.add(planetFriezaPivot);
+//Add a Sphere (frieza's moon)
+fmoon79 = new SphereGeometry(1, 10, 10);
+moon79Material = new LambertMaterial({ color: 0xF8E0E0, map: THREE.ImageUtils.loadTexture("../../Assets/Textures/Planets/foam.jpg") });
+fmoon79 = new Mesh(fmoon79, moon79Material);
+fmoon79.castShadow = true;
+fmoon79.position.x = 0;
+fmoon79.position.y = 0;
+fmoon79.position.z = 7;
+moon79.add(fmoon79);
+planetFrieza.add(moon79);
+// Add an AmbientLight to the scene
+ambientLight = new AmbientLight(0x0c0c0c);
+scene.add(ambientLight);
+// add controls
+gui = new GUI();
+control = new Control(0.0, 0.0, 0.0);
+addControl(control);
+// Add framerate stats
+addStatsObject();
+console.log("Added Stats to scene...");
+document.body.appendChild(renderer.domElement);
+// render the scene	
+gameLoop();
+window.addEventListener('resize', onResize, false);
 function onResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 function addControl(controlObject) {
-    /*    colorPicker = gui.addColor( colorConfig, 'color').onChange(
-            function(getColor){
-                //getColor=getColor.replace( '#','0x' );
-                sunMaterial.color =  new THREE.Color(getColor);
-                vegetaMaterial.color = new THREE.Color(getColor);
-                namekMaterial.color = new THREE.Color(getColor);
-                kaiMaterial.color =  new THREE.Color(getColor);
-                heraMaterial.color = new THREE.Color(getColor);
-                friezaMaterial.color =  new THREE.Color(getColor);
-                moon79Material.color = new THREE.Color(getColor);
-                leftFootMaterial.color =  new THREE.Color(getColor);
-                rightFootMaterial.color = new THREE.Color(getColor);
-                console.log(getColor);
-            });*/
     gui.add(controlObject, 'focusMoon');
     gui.add(controlObject, 'viewSolarSystem');
     //gui.add(controlObject, 'removesphere');
@@ -241,12 +211,6 @@ function addStatsObject() {
 function gameLoop() {
     stats.update();
     cameraControls.update();
-    /*   scene.traverse(function(threeObject:THREE.Object3D) {
-            if (threeObject == blobbyBoy) {
-                threeObject.rotation.x += control.rotationSpeedX;
-                threeObject.rotation.y += control.rotationSpeedY;
-                threeObject.rotation.z += control.rotationSpeedZ;
-        });*/
     planetVegeta.rotation.y += 0.01;
     planetVegeta.rotation.z -= 0.002;
     planetKai.rotation.y += 0.008;
@@ -257,8 +221,10 @@ function gameLoop() {
     planetNamek.rotation.z += 0.007;
     planetFrieza.rotation.y += 0.009;
     planetFriezaPivot.rotation.y += 0.005;
+    //update position for camera tracking
     scene.updateMatrixWorld();
     focusVector.setFromMatrixPosition(planetFrieza.matrixWorld);
+    //boolean for following planet w/ moon, prob do a dropdown later for tracking other planets
     if (isFollowingMoonPlanet == true) {
         camera.lookAt(focusVector);
     }
--- Scripts/objects/control.js
+++ Scripts/objects/control.js
@@ -4,25 +4,19 @@ var objects;
     // CONTROL CLASS ++++++++++++++++++++++++++++++++++++++++++
     var Control = (function () {
         // PUBLIC INSTANCE VARIABLES
-        /*public rotationSpeedX: number;
-        public rotationSpeedY: number;
-        public rotationSpeedZ: number;*/
-        //public numberOfObjects: number;
         // CONSTRUCTOR ++++++++++++++++++++++++++++++++++++++++
         function Control(planeWidth, planeHeight) {
-            /*this.rotationSpeedX = rotationSpeedX;
-            this.rotationSpeedY = rotationSpeedY;
-            this.rotationSpeedZ = rotationSpeedZ;*/
-            //this.numberOfObjects = scene.children.length;
             this._planeWidth = planeWidth;
             this._planeHeight = planeHeight;
         }
         //PUBLIC METHODS ++++++++++++++++++++++++++++++++++++++++
         //Change Camera Method
+        //focus on the one with the moon
         Control.prototype.focusMoon = function () {
             camera.position.set(planetFrieza.position.x, planetFrieza.position.y + 30, planetFrieza.position.z - 50);
             isFollowingMoonPlanet = true;
         };
+        //focus back on the entire solar system
         Control.prototype.viewSolarSystem = function () {
             isFollowingMoonPlanet = false;
             camera.position.set(-70, 2, 2);
